c62206c30011fdef51bcee66cdeb0ef666248d71
Mocky <arnaud.solier@outlook.fr>
2022-10-28 12:24:55 +0200

    mise en place de la page html / css et php

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo 1;
+<head>
+    <?php require_once 'includes/head.php' ?>
+    <title>Projet TODO</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <?php require_once 'includes/header.php' ?>
+        <div class="content">
+            <div class="todo-container">
+                <h1>Ma Todo liste</h1>
+                <div class="todo-form"></div>
+                <div class="todo-list"></div>
+            </div>
+        </div>
+        <?php require_once 'includes/footer.php' ?>
+    </div>
+
+</body>
+
+</html>
